02e3893a14c5105f454599525286c3a51795631d
YPC <ypc@example.com>
2022-03-27 01:45:56 +1100

    update

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test(){
-        return "Welcome to Jenkins";
+        return "Welcome to Jenkins on Prod";
     }
 }
