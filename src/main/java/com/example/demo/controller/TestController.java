bd6a8a94c6fad4e5e21b9951d9a2b00319d4c7dd
YPC <ypc@example.com>
2022-03-29 18:18:34 +1100

    update

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test(){
-        return "Welcome to Jenkins on Staging with pipeline";
+        return "Welcome to Docker";
     }
 }
